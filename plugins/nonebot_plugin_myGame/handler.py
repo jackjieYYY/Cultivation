f4e1842f46e7b870860fffefaadb37e552fc63f0
jackjieYYY <53422254+jackjieYYY@users.noreply.github.com>
2024-12-16 12:14:25 +0000

    1

--- plugins/nonebot_plugin_myGame/handler.py
+++ plugins/nonebot_plugin_myGame/handler.py
@@ -2,7 +2,7 @@ from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent, MessageSegment
 from nonebot import on_message
 from nonebot.rule import Rule
 import time
-from plugins.nonebot_plugin_myGame.consts import MY_PRIVATE_GROUP_ID
+from plugins.nonebot_plugin_myGame.consts import MY_PRIVATE_GROUP_ID, TARGET_KEYWORDS
 from plugins.nonebot_plugin_myGame.person import Person
 
 # 创建一个群消息的监听器
@@ -16,6 +16,9 @@ group_message_handler = on_message(
 async def handle_group_message(bot: Bot, event: GroupMessageEvent):
     person = Person.getPerson(bot.self_id)
     if not person:
+        return
+    message_content = str(event.message)
+    if TARGET_KEYWORDS not in message_content:
         return
     await person.training()
     return

4a9733e0006cd03cf3c2445c2a622dd9e4efc0d7
jackjieYYY <53422254+jackjieYYY@users.noreply.github.com>
2024-12-16 12:07:58 +0000

    MY_PRIVATE_GROUP_ID

--- plugins/nonebot_plugin_myGame/handler.py
+++ plugins/nonebot_plugin_myGame/handler.py
@@ -2,12 +2,12 @@ from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent, MessageSegment
 from nonebot import on_message
 from nonebot.rule import Rule
 import time
-from plugins.nonebot_plugin_myGame.consts import TARGET_GROUP_ID
+from plugins.nonebot_plugin_myGame.consts import MY_PRIVATE_GROUP_ID
 from plugins.nonebot_plugin_myGame.person import Person
 
 # 创建一个群消息的监听器
 group_message_handler = on_message(
-    rule=Rule(lambda event: isinstance(event, GroupMessageEvent) and event.group_id == TARGET_GROUP_ID),
+    rule=Rule(lambda event: isinstance(event, GroupMessageEvent) and event.group_id == MY_PRIVATE_GROUP_ID),
     priority=5,
     block=False
 )
